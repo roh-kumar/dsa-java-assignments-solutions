0fc5a5433381163fbdb05c71d8d291a669685ee0
roh-kumar <roh.dhiman@gmail.com>
2021-10-26 23:31:15 +0530

    feat: add searching programs medium Q.1, Q.3, Q.5, Q.6, Q.15

---
+++ 06-searching/02-medium/Q_01_First&LastPositionOfElementInSortedArray.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        ans[0] = searchPosition(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = searchPosition(nums, target, false);
+        }
+        return ans;
+    }
+    
+    int searchPosition(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            }
+            else if (target > nums[mid]) {
+                start = mid + 1;
+            }
+            else {
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
