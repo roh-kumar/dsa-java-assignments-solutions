1d3ca96f57aba9ab1531c4b1e6a4714e294967ab
roh-kumar <roh.dhiman@gmail.com>
2021-11-03 23:24:22 +0530

    feat: add sorting programs easy Q.30

---
+++ 07-sorting/01-Easy/Q_30_2Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i = 1; i < nums.length; i++){
+            for(int j = i; j < nums.length; j++){
+                if(nums[j - i] + nums[j] == target){
+                    result[0] = j - i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
